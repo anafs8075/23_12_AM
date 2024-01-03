2764614d832f0c683bab79a2d3e0d2612084a833
anafs8075 <anafs8075@gmail.com>
2024-01-03 12:28:48 +0900

    arcicles, members 이전, App 클래스 내부에서 하던 기능 실행을 각각의 컨트롤러 내부로 이전, App 간소화

--- src/com/KoreaIT/java/AM/App.java
+++ src/com/KoreaIT/java/AM/App.java
@@ -1,23 +1,13 @@
 package com.KoreaIT.java.AM;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import com.KoreaIT.java.AM.controller.ArticleController;
+import com.KoreaIT.java.AM.controller.Controller;
 import com.KoreaIT.java.AM.controller.MemberController;
-import com.KoreaIT.java.AM.dto.Article;
-import com.KoreaIT.java.AM.dto.Member;
 
 public class App {
 
-	List<Article> articles;
-	List<Member> members;
-
-	public App() {
-		articles = new ArrayList<>();
-		members = new ArrayList<>();
-	}
 	
 
 	public void run() {
@@ -25,8 +15,10 @@ public class App {
 
 		Scanner sc = new Scanner(System.in);
 
-		MemberController memberController = new MemberController(sc, members);
-		ArticleController articleController = new ArticleController(sc, articles);
+		MemberController memberController = new MemberController(sc);
+		ArticleController articleController = new ArticleController(sc);
+
+		Controller controller = null;
 		articleController.makeTestData();
 		
 		while (true) {
@@ -39,23 +31,26 @@ public class App {
 			if (cmd.equals("exit")) {
 				break;
 			}
-			if (cmd.equals("member join")) {
+			String[] cmdBits = cmd.split(" ");
 
-				memberController.doJoin();
-				
-			} else if (cmd.equals("article write")) {
-				articleController.doWrite();
-			} else if (cmd.startsWith("article list")) {
-				articleController.showList(cmd);
-			} else if (cmd.startsWith("article detail")) {
-				articleController.showDetail(cmd);
-			} else if (cmd.startsWith("article delete")) {
-				articleController.doDelete(cmd);
-			} else if (cmd.startsWith("article modify")) {
-				articleController.doModify(cmd);
+			String controllerName = cmdBits[0];
+
+			if (cmdBits.length == 1) {
+				System.out.println("명령어를 확인해줘2");
+				continue;
+			}
+
+			String actionMethodName = cmdBits[1];
+
+			if (controllerName.equals("article")) {
+				controller = articleController;
+			} else if (controllerName.equals("member")) {
+				controller = memberController;
 			} else {
 				System.out.println("사용할 수 없는 명령어입니다");
+				continue;
 			}
+			controller.doAction(actionMethodName, cmd);
 		}
 		System.out.println("== 프로그램 끝 == ");
 
