24f630a6d44862228802251a402015f968c7edc8
anafs8075 <anafs8075@gmail.com>
2024-01-03 10:42:39 +0900

    MemberController 추가

--- src/com/KoreaIT/java/AM/App.java
+++ src/com/KoreaIT/java/AM/App.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.KoreaIT.java.AM.controller.ArticleController;
+import com.KoreaIT.java.AM.controller.MemberController;
 import com.KoreaIT.java.AM.dto.Article;
 import com.KoreaIT.java.AM.dto.Member;
 import com.KoreaIT.java.AM.util.Util;
@@ -17,6 +19,7 @@ public class App {
 		articles = new ArrayList<>();
 		members = new ArrayList<>();
 	}
+	
 
 	public void run() {
 		System.out.println("== 프로그램 시작 == ");
@@ -24,7 +27,10 @@ public class App {
 		makeTestData();
 		Scanner sc = new Scanner(System.in);
 		int lastArticleId = 3;
-		int lastMemberId = 0;
+
+		MemberController memberController = new MemberController(sc, members);
+		
+		
 		while (true) {
 			System.out.print("명령어 > ");
 			String cmd = sc.nextLine().trim();
@@ -36,38 +42,11 @@ public class App {
 				break;
 			}
 			if (cmd.equals("member join")) {
-				System.out.println("==회원 가입==");
-				int id = lastMemberId + 1;
-				String regDate = Util.getNowDate_TimeStr();
-				String loginId = null;
-				while (true) {
-					System.out.print("로그인 아이디 : ");
-					loginId = sc.nextLine();
-					if (isJoinableLoginId(loginId) == false) {
-						System.out.println("이미 사용중이야");
-						continue;
-					}
-					break;
-				}
-				String loginPw = null;
-				while (true) {
-					System.out.print("로그인 비밀번호 : ");
-					loginPw = sc.nextLine();
-					System.out.print("로그인 비밀번호 확인: ");
-					String loginPwConfirm = sc.nextLine();
-					if (loginPw.equals(loginPwConfirm) == false) {
-						System.out.println("비밀번호 다시 확인해");
-						continue;
-					}
-					break;
-				}
-				System.out.print("이름 : ");
-				String name = sc.nextLine();
-				Member member = new Member(id, regDate, loginId, loginPw, name);
-				members.add(member);
-				System.out.printf("%d번 회원이 가입 되었습니다. %s님 환영합니다.\n", id, name);
-				lastMemberId++;
+
+				memberController.doJoin();
+				
 			} else if (cmd.equals("article write")) {
+				
 				System.out.println("==게시글 작성==");
 				int id = lastArticleId + 1;
 				String regDate = Util.getNowDate_TimeStr();
@@ -184,14 +163,6 @@ public class App {
 
 	}
 
-	private boolean isJoinableLoginId(String loginId) {
-		for (Member member : members) {
-			if (member.getLoginId().equals(loginId)) {
-				return false;
-			}
-		}
-		return true;
-	}
 
 	private Article getArticleById(int id) {
 		for (Article article : articles) {
